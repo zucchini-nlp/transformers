eb88a4c15f3a4e04b71ed4ecb51d14902b934ca2
raushan <raushan@huggingface.co>
2026-02-02 13:49:54 +0100

    make comment more detailed for future us

--- src/transformers/modeling_backbone_utils.py
+++ src/transformers/modeling_backbone_utils.py
@@ -263,21 +263,24 @@ def consolidate_backbone_kwargs_to_config(
     timm_default_kwargs: dict | None = None,
     **kwargs,
 ):
+    # Lazy import to avoid circular import issues. Can be imported properly
+    # after deleting ref to `BackboneMixin` in `utils/backbone_utils.py`
     from .configuration_utils import PreTrainedConfig
     from .models.auto import CONFIG_MAPPING
 
-    use_timm_backbone = kwargs.pop("use_timm_backbone", True)  # this is not always `True`
+    use_timm_backbone = kwargs.pop("use_timm_backbone", True)
     backbone_kwargs = kwargs.pop("backbone_kwargs", {})
     backbone = kwargs.pop("backbone") if kwargs.get("backbone") is not None else default_backbone
     kwargs.pop("use_pretrained_backbone", None)
 
-    # Init timm backbone with hardcoded values for BC
+    # Init timm backbone with hardcoded values for BC. If everything is set to `None` and there is
+    # a default timm config, we use it to init the backbone.
     if (
         timm_default_kwargs is not None
         and use_timm_backbone
         and backbone is not None
         and backbone_config is None
-        and backbone_kwargs is None
+        and not backbone_kwargs
     ):
         backbone_config = CONFIG_MAPPING["timm_backbone"](backbone=backbone, **timm_default_kwargs)
     elif backbone is not None and backbone_config is None:
